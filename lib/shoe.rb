0931f372e850ca4df0ed4013dbda08f34d36750a
Damian Malysza <damian.malysza@gmail.com>
2020-10-27 23:16:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless brand.include?(brands)
+    BRANDS << brand unless BRANDS.include?(brands)
   end
 
   def cobble
